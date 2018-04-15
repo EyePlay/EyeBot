77b93442958f80695b1fe19f8e26b5151045138c
EyePlay <kyle.offenbacher@gmail.com>
2018-04-15 11:29:35 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,14 +3,9 @@ const bot = new Discord.Client();
 
 bot.on('message', (message) => {
 
-    if(message.content == 'who is fake') {
-        message.reply('you are fake');
-        //message.channel.sendMessage('pong')
-    }
-
-    else if(message.content == 'ok') {
-        message.reply('actually, it is not ok');
-    }
+    if(message.content == 'ping') {
+        message.channel.sendMessage('pong')
+    }   
 });
 
 client.login(process.env.BOT_TOKEN);
