35d859047b8b72209986694c34f6cce34265e151
EyePlay <kyle.offenbacher@gmail.com>
2018-04-15 10:39:38 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('message', (message) => {
+
+    if(message.content == 'who is fake') {
+        message.reply('you are fake');
+        //message.channel.sendMessage('pong')
+    }
+
+    else if(message.content == 'ok') {
+        message.reply('actually, it is not ok');
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
+
