b0c3417bb5bdc9f399441d83678ec202daf37b65
EyePlay <kyle.offenbacher@gmail.com>
2018-04-15 16:49:54 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,8 +9,8 @@ client.on('message', message => {
     if (message.content == 'ping') {
     	message.reply('pong');
   	}
-    else if(message.content == 'who is fake') {
-        message.channel.sendMessage('@264952723030409217 is fake');
+     else if(message.content == 'who is fake') {
+        message.channel.sendMessage({to: 432689272106123277, message: `${user.mention(264952723030409217)} is fake`});
     }
 });
 
